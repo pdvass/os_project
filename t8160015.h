361fc42d9c4f4bf5578d2b53b4689966b9197bca
pdvass <pantelovas@gmail.com>
2022-05-07 19:34:43 +0300

    Rearranging vars

---
+++ t8160015.h
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <pthread.h>
+
+// Constant variables
+#define N_TEL 3 // Thlefonhtes
+#define N_CASH 2 // Cashiers
+#define N_SEAT 10 
+#define N_ZONE_SEATS 10
+#define N_ZONE_A 10
+#define N_ZONE_B 20
+#define P_ZONE_A 0.3 // Probability to choose zone a
+#define C_ZONE_A 30 // Cost of zone A
+#define C_ZONE_B 20 // Cost of zone A
+#define N_SEAT_LOW 1 
+#define N_SEAT_HIGH 5
+
+// All Ts need srand
+#define T_RES_LOW 1
+#define T_RES_HIGH 5
+#define T_SEAT_LOW 5
+#define T_SEAT_HIGH 13
+#define T_CASH_LOW 4
+#define T_CASH_HIGH 8
+#define P_CARD_SUCCESS 0.9
